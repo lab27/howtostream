d3eae71f6bdef5f4ccc3e971633e4b3e7820b6a8
Nick Niles <nickyniles@gmail.com>
2015-05-19 23:58:43 +0200

    initial commit

---
+++ app/js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var app = (function(document, $) {
+	var docElem = document.documentElement,
+		_userAgentInit = function() {
+			docElem.setAttribute('data-useragent', navigator.userAgent);
+		},
+		_init = function() {
+			$(document).foundation();
+            // needed to use joyride
+            // doc: http://foundation.zurb.com/docs/components/joyride.html
+            $(document).on('click', '#start-jr', function () {
+                $(document).foundation('joyride', 'start');
+            });
+			_userAgentInit();
+		};
+	return {
+		init: _init
+	};
+})(document, jQuery);
+
+(function() {
+	app.init();
+})();
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
