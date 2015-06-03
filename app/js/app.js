e1643d545889dee48ed9cad8a91ea8731672c59f
Nick Niles <nickyniles@gmail.com>
2015-06-03 11:26:40 +0200

    added sidenav

--- app/js/app.js
+++ app/js/app.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var app = (function(document, $) {
-	var docElem = document.documentElement,
-		_userAgentInit = function() {
-			docElem.setAttribute('data-useragent', navigator.userAgent);
-		},
-		_init = function() {
-			$(document).foundation();
-            // needed to use joyride
-            // doc: http://foundation.zurb.com/docs/components/joyride.html
-            $(document).on('click', '#start-jr', function () {
-                $(document).foundation('joyride', 'start');
-            });
-			_userAgentInit();
-		};
-	return {
-		init: _init
-	};
-})(document, jQuery);
+// var app = (function(document, $) {
+// 	var docElem = document.documentElement,
+// 		_userAgentInit = function() {
+// 			docElem.setAttribute('data-useragent', navigator.userAgent);
+// 		},
+// 		_init = function() {
+// 			$(document).foundation();
+//             // needed to use joyride
+//             // doc: http://foundation.zurb.com/docs/components/joyride.html
+//             $(document).on('click', '#start-jr', function () {
+//                 $(document).foundation('joyride', 'start');
+//             });
+// 			_userAgentInit();
+// 		};
+// 	return {
+// 		init: _init
+// 	};
+// })(document, jQuery);
 
-(function() {
-	app.init();
-})();
+// (function() {
+// 	app.init();
+// })();
 
 
 $(function() {
@@ -37,4 +37,19 @@ $(function() {
       }
     }
   });
+});
+
+var $magellan = 'magellan-expedition';
+$(document).foundation({
+  $magellan : {
+    // specify the class used for active sections
+    active_class: 'reading',
+    // how many pixels until the magellan bar sticks, 0 = auto
+    threshold: 300,
+    fixed_top: 40,
+    // pixels from the top of destination for it to be considered active
+    destination_threshold: 20,
+    // calculation throttling to increase framerate
+    throttle_delay: 50 
+  },
 });
